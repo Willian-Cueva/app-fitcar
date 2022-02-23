2265206d99e8680a152f6ab97d5415d8a15668b4
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2022-02-23 12:38:33 -0500

    intento de una peticion api :(

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,14 @@
+import 'package:fit_car/models/reqres_model.dart';
 import 'package:fit_car/src/bloc/auth_cubit.dart';
 import 'package:fit_car/widgets/mi_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+
+Future<ReqResrespuesta> getUsuarios() async {
+  final resp = await http.get(Uri.parse('https://reqres.in/api/users'));
+  return reqResrespuestaFromJson(resp.body);
+}
 
 class Home extends StatelessWidget {
   static Widget create(BuildContext context) => Home();
@@ -18,7 +25,7 @@ class Home extends StatelessWidget {
           final authUser = (state as AuthSignedIn).user;
           email = authUser.email;
           return Center(
-              child: Column(
+              child: ListView(
             children: <Widget>[
               Text('User: ${authUser.uid}'),
               SizedBox(
@@ -31,10 +38,44 @@ class Home extends StatelessWidget {
                 child: Icon(Icons.add_reaction_rounded),
                 onPressed: () => print(email),
               ),
+              FutureBuilder(
+                builder: (BuildContext context,
+                    AsyncSnapshot<ReqResrespuesta> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    return _ListaUsuarios(snapshot.data!.data);
+                  }
+                },
+                future: getUsuarios(),
+              )
             ],
           ));
         },
       ),
     );
+  }
+}
+
+class _ListaUsuarios extends StatelessWidget {
+  final List<Usuario> usuarios;
+
+  _ListaUsuarios(this.usuarios);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView.builder(
+        itemCount: usuarios.length,
+        itemBuilder: (BuildContext context, int index) {
+          final usuario = usuarios[index];
+          return ListTile(
+            title: Text('${usuario.firstName} ${usuario.lastName}'),
+            subtitle: Text(usuario.email),
+            trailing: Image.network(usuario.avatar),
+          );
+        });
   }
 }
