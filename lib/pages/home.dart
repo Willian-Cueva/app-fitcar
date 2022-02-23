02ffab01b41b7f2f99a55d9925755d78dfcb1e97
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2022-02-23 12:55:00 -0500

    other video

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,13 @@ import 'package:fit_car/src/bloc/auth_cubit.dart';
 import 'package:fit_car/widgets/mi_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+
+Future<int> getStatusCode() async {
+  var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
+  http.Response response = await http.get(url);
+  return response.statusCode;
+}
 
 class Home extends StatelessWidget {
   static Widget create(BuildContext context) => Home();
